de0ff91c586cadd27e81ee892a9e5743ed410a51
saurabhcse15 <saurabh.yadav.cse15@iitbhu.ac.in>
2017-06-02 00:31:00 +0530

    Update and rename fast_factorization_using_seive to fast_factorization_using_seive.cpp

---
+++ maths/fast_factorization_using_seive.cpp
@@ -0,0 +1,24 @@
+int minPrime[num + 1];
+void seive(){
+    for (int i = 2; i * i <= num; ++i) {
+        if (minPrime[i] == 0) { //if i is prime
+            for (int j = i * i; j <= num; j += i) {
+                if (minPrime[j] == 0) {
+                    minPrime[j] = i;
+                }
+            }
+        }
+    }
+    for (int i = 2; i <= num; ++i) {
+        if (minPrime[i] == 0) {
+            minPrime[i] = i;
+        }
+    }
+}
+void primefactor(int n) {
+    while (n != 1) {
+        //res.push_back(minPrime[n]);
+        cout<<minPrime[n]<<endl;
+        n /= minPrime[n];
+    }
+}
