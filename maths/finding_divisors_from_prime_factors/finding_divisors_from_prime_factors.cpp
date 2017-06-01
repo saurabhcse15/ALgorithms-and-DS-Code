af229bca3427eae2c6b09b03f33f50192be8c819
saurabhcse15 <saurabh.yadav.cse15@iitbhu.ac.in>
2017-06-02 00:48:00 +0530

    Update finding_divisors_from_prime_factors.cpp

--- maths/finding_divisors_from_prime_factors/finding_divisors_from_prime_factors.cpp
+++ maths/finding_divisors_from_prime_factors/finding_divisors_from_prime_factors.cpp
@@ -1,1 +1,13 @@
-
+vector <pair<int,int> > primefactor;  //it will store prime factors and their count
+                                      //primefactors can be generated using fast_factorization and other known algo
+void setDivisors(int n, int i) {      // default value of n and i are 1 and 0
+    int j, x, k;
+    for (j = i; j<primefactor.size(); j++) 
+    {   x = primefactor[j].first * n;
+        for (k = 0; k<primefactor[j].second; k++) 
+        {   divisors.push_back(x);
+            setDivisors(x, j + 1);
+            x *= primefactor[j].first;
+        }
+    }
+}
